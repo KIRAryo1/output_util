d6fe19f5cd70bbae8140c9ef4cc50c6276851a2d
KIRAryo1 <r.kira@grooves.co.jp>
2018-11-12 17:10:26 +0900

    Output filename comes from input filename

--- sb2md.rb
+++ sb2md.rb
@@ -1,8 +1,14 @@
-filename = 'machine_learning.in'
+filename = ARGV[0] || 'machine_learning.in'
+outfile = filename.split('.')[0] + '.out'
+txt = ""
 
 File.open(filename, "r") do |io|
   txt = io.read
 
-  first_line = txt.split("\n")[0]
-  p first_line
+  first_line = txt.split("\n")
+  p first_line[0]
+end
+
+File.open(outfile, "w") do |io|
+  io.puts txt
 end
