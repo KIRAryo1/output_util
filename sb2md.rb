649be430d3b675f03788708a804bf2606535a144
KIRAryo1 <r.kira@grooves.co.jp>
2018-11-12 18:15:12 +0900

    [* hoge] into __hoge__

--- sb2md.rb
+++ sb2md.rb
@@ -1,3 +1,11 @@
+def headline_size(str)
+  matches = str.match(/\[\* (.+)\]/)
+  ret = matches ? "__" + matches[1] + "__" : str
+  p ret
+  return ret
+end
+
+
 filename = ARGV[0] || 'machine_learning.in'
 outfile = filename.split('.')[0] + '.out'
 txt = ""
@@ -5,10 +13,15 @@ txt = ""
 File.open(filename, "r") do |io|
   txt = io.read
 
-  first_line = txt.split("\n")
-  p first_line[0]
+  lines = txt.split("\n")
+
+  lines = lines.map{|e| headline_size(e)}
+
+  txt = lines.join("\n")
 end
 
 File.open(outfile, "w") do |io|
   io.puts txt
 end
+
+# p txt
