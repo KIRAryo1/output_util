c7962bac06a5cc5c3c7242b091301fa2ce1c4f18
KIRAryo1 <r.kira@grooves.co.jp>
2018-11-13 11:21:37 +0900

    [$ ] as line into 20443 20443, [$ ] in line into $ $

--- sb2md.rb
+++ sb2md.rb
@@ -1,16 +1,19 @@
 def headline_bold(str)
-  matches = str.match(/\[\* (.+?)\]/)
-  return matches ? "__" + matches[1] + "__" : str
+  str.gsub(/\[\* (.+?)\]/, '__\1__')
 end
 
 def inline_bold(str)
-  matches = str.match(/\[\[(.+?)\]\]/)
-  return matches ? "__" + matches[1] + "__" : str
+  str.gsub(/\[\[(.+?)\]\]/, '__\1__')
 end
 
 def headline_size(str)
   matches = str.match(/\[(\*\*+) (.+?)\]/)
   return matches ? "#" * (7 - matches[1].length) + " " + matches[2] : str
+end
+
+def numerical_formula(str)
+  # str.gsub(/\[\$ ([^\[]+\[.+?\][^\]]+?|.+?)\]/, '$$ \1 $$')
+  str.gsub(/^\[\$ (.+?)\]$/, '$$ \1 $$').gsub(/\[\$ (.+?)\]/, '$ \1 $')
 end
 
 
@@ -26,12 +29,12 @@ File.open(filename, "r") do |io|
   lines = lines.map{|e| headline_bold(e)}
   lines = lines.map{|e| inline_bold(e)}
   lines = lines.map{|e| headline_size(e)}
+  lines = lines.map{|e| numerical_formula(e)}
 
+  # markdown new line needs 2 \n's.
   txt = lines.join("\n\n")
 end
 
 File.open(outfile, "w") do |io|
   io.puts txt
 end
-
-# p txt
